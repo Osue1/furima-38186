403bc12363317a2d6b79c9c5545e5026db222663
Osue.Kodai <kodai.osue@gmail.com>
2022-10-15 19:09:52 +0900

    正規表現の適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,18 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_reading, presence: true
-  validates :first_name_reading, presence: true
+  # validates :password, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/i} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
+    validates :last_name_reading
+    validates :first_name_reading
+  end
+
   validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
