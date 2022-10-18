2fc7a2b500f26dbe54d11021ae6dab849bd05538
Osue.Kodai <kodai.osue@gmail.com>
2022-10-18 17:43:25 +0900

    productモデルにバリデーションの追記

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true, foreign_key: true
+
+  belongs_to :user
 end
