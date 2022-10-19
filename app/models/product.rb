ac58e2c54c2c4e19179d2094dcb363eb91626bd7
Osue.Kodai <kodai.osue@gmail.com>
2022-10-20 07:15:43 +0900

    ActiveHash各モデルの作成とアソシエーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,20 @@ class Product < ApplicationRecord
   validates :days_to_ship_id, presence: true
   validates :price, presence: true
   validates :user, presence: true, foreign_key: true
+  validates :category_id , numericality: { other_than: 1 }
+  validates :status_id , numericality: { other_than: 1 }
+  validates :postage_id , numericality: { other_than: 1 }
+  validates :prefecture_id , numericality: { other_than: 1 }
+  validates :days_to_ship_id , numericality: { other_than: 1 }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
   has_one_attached :image
+
 end
