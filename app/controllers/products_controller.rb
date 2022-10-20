108df5d51bb3784bc3dd3b82df8cf4bf84cd9fda
Osue.Kodai <kodai.osue@gmail.com>
2022-10-20 21:34:40 +0900

    各モデルの修正と出品(データベースへ保存)機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,21 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :explanation, :category_id, :status_id, :postage_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,14 @@
 class Product < ApplicationRecord
+  validates :image,presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :days_to_ship_id, presence: true
-  validates :price, presence: true
-  validates :user, presence: true, foreign_key: true
-  validates :category_id , numericality: { other_than: 1 }
-  validates :status_id , numericality: { other_than: 1 }
-  validates :postage_id , numericality: { other_than: 1 }
-  validates :prefecture_id , numericality: { other_than: 1 }
-  validates :days_to_ship_id , numericality: { other_than: 1 }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
+  validates :user, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
