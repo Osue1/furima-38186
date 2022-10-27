63f6e0452434f95350a42dc3932d1c2b67407f8e
Osue.Kodai <kodai.osue@gmail.com>
2022-10-28 05:18:52 +0900

    jsで全角でも手数料計算できるように設定

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,7 +2,13 @@ function load_window (){
   const priceForm = document.getElementById("item-price");
   
   priceForm.addEventListener('keyup', () => {
-    const price = priceForm.value;
+    
+    function numberExchange(str) {
+      return str.replace(/[０-９]/g, function(s) {
+          return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+      });
+    }
+    const price = numberExchange(priceForm.value);
     const taxForm = document.getElementById("add-tax-price");
     const profitForm = document.getElementById("profit");
     taxForm.innerHTML = Math.floor(price / 10);
