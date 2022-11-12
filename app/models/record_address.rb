4c01b86c12e9df6923ed3aa6f0e844b2a836932d
Osue.Kodai <kodai.osue@gmail.com>
2022-11-12 21:37:10 +0900

    正規表現によるバリデーション

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -3,11 +3,11 @@ class RecordAddress
   attr_accessor :user_id, :product_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}/ } 
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}/ } 
     validates :user_id
     validates :product_id
     validates :token
